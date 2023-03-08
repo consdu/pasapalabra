bfe5d93f7a645e8a1d33f6c2d8ec79fcc0308088
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-03-08 16:22:57 +0100

    make start screen

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ import { allQuestions, getGameQuestions } from './questionsDB.js';
 const questionText = document.querySelector('.question');
 const userAnswer = document.querySelector('.user-answer');
 const skipButton = document.querySelector('.skip');
-const userForm = document.querySelector('.user-form');
+const gameForm = document.querySelector('.game-form');
 
 const letters = document.querySelectorAll('.letter');
 
-const showQuestion = (allIndexes, currentIndex, gameQuestions) => {
+const showQuestion = (allIndexes, currentIndex, gameQuestions, score) => {
+  if (allIndexes.length === 0) {
+    alert(score.correctAnswers, score.wrongAnswers);
+    return;
+  }
   letters.forEach((letter) => letter.classList.remove('active'));
   letters[allIndexes[currentIndex]].classList.add('active');
   userAnswer.value = '';
@@ -18,8 +22,10 @@ const showQuestion = (allIndexes, currentIndex, gameQuestions) => {
 
 const playGame = () => {
   const gameQuestions = getGameQuestions(allQuestions);
-  let correctAnswers = 0;
-  let wrongAnswers = 0;
+  const score = {
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  };
   let allIndexes = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     21, 22, 23, 24, 25, 26,
@@ -27,16 +33,18 @@ const playGame = () => {
   let currentIndex = 0;
 
   skipButton.addEventListener('click', () => {
+    if (allIndexes.length === 0) return;
     if (currentIndex >= allIndexes.length - 1) {
       currentIndex = 0;
-      return showQuestion(allIndexes, currentIndex, gameQuestions);
+      return showQuestion(allIndexes, currentIndex, gameQuestions, score);
     }
     currentIndex++;
-    return showQuestion(allIndexes, currentIndex, gameQuestions);
+    return showQuestion(allIndexes, currentIndex, gameQuestions, score);
   });
 
-  userForm.addEventListener('submit', (event) => {
+  gameForm.addEventListener('submit', (event) => {
     event.preventDefault();
+    if (allIndexes.length === 0) return;
     if (
       userAnswer.value.toLowerCase() ===
       gameQuestions[allIndexes[currentIndex]].answer
@@ -44,8 +52,8 @@ const playGame = () => {
       gameQuestions[allIndexes[currentIndex]].status = 1;
       letters[allIndexes[currentIndex]].classList.toggle('correct-answer');
       allIndexes.splice(currentIndex, 1);
-      correctAnswers++;
-      return showQuestion(allIndexes, currentIndex, gameQuestions);
+      score.correctAnswers++;
+      return showQuestion(allIndexes, currentIndex, gameQuestions, score);
     }
     if (
       userAnswer.value.toLowerCase() !==
@@ -54,12 +62,12 @@ const playGame = () => {
       gameQuestions[allIndexes[currentIndex]].status = 0;
       letters[allIndexes[currentIndex]].classList.toggle('wrong-answer');
       allIndexes.splice(currentIndex, 1);
-      wrongAnswers++;
-      return showQuestion(allIndexes, currentIndex, gameQuestions);
+      score.wrongAnswers++;
+      return showQuestion(allIndexes, currentIndex, gameQuestions, score);
     }
   });
 
-  showQuestion(allIndexes, currentIndex, gameQuestions);
+  showQuestion(allIndexes, currentIndex, gameQuestions, score);
 };
 
 playGame();
